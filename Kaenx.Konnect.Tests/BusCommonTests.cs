a486340dc36e05fe58bd4635581451aebb74ff88
Dennis <ostermann3k@googlemail.com>
2026-02-26 22:29:08 +0100

    Added Tests for BusCommon

---
+++ Kaenx.Konnect.Tests/BusCommonTests.cs
@@ -0,0 +1,25 @@
+﻿using Kaenx.Konnect.Classes;
+using System.Net;
+using Kaenx.Konnect.Addresses;
+using Xunit;
+
+namespace Kaenx.Konnect.Tests
+{
+    public class BusCommonTests
+    {
+        [Fact]
+        public async Task Add_ReturnsCorrectSum()
+        {
+            var endpoint = new IPEndPoint(IPAddress.Parse("192.168.178.167"), 3671);
+
+            var conn = KnxFactory.CreateTunnelingUdp(endpoint);
+            await conn.Connect();
+            var sut = new BusCommon(conn); 
+
+            MulticastAddress ga = MulticastAddress.FromString("1/4/53");
+            byte data = 0x00;
+            await sut.GroupValueWrite(ga, data);
+        }
+    }
+}
+
