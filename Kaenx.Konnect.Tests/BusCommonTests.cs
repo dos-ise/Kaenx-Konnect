22d032a8aa19065a6f42372224f47bf1b7e1d59e
Dennis <ostermann3k@googlemail.com>
2026-02-27 08:41:21 +0100

    added test for GroupValueWrite array

--- Kaenx.Konnect.Tests/BusCommonTests.cs
+++ Kaenx.Konnect.Tests/BusCommonTests.cs
@@ -8,7 +8,7 @@ namespace Kaenx.Konnect.Tests
     public class BusCommonTests
     {
         [Fact]
-        public async Task Add_ReturnsCorrectSum()
+        public async Task GroupValueWrite_SendsPercentage_false()
         {
             var endpoint = new IPEndPoint(IPAddress.Parse("192.168.178.167"), 3671);
 
@@ -19,6 +19,22 @@ namespace Kaenx.Konnect.Tests
             MulticastAddress ga = MulticastAddress.FromString("1/4/53");
             byte data = 0x00;
             await sut.GroupValueWrite(ga, data);
+        }
+
+        [Fact]
+        public async Task GroupValueWrite_SendsByteArray()
+        {
+            var endpoint = new IPEndPoint(IPAddress.Parse("192.168.178.167"), 3671);
+            var conn = KnxFactory.CreateTunnelingUdp(endpoint);
+            await conn.Connect();
+
+            var sut = new BusCommon(conn);
+            MulticastAddress ga = MulticastAddress.FromString("1/4/58");
+
+            byte[] data = new byte[] { 0x33 }; // 20% - DPT 5.001
+            await sut.GroupValueWrite(ga, data);
+
+            await conn.Disconnect();
         }
     }
 }
