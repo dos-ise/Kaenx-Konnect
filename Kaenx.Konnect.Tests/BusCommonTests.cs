743df51c6e532edc4d0ce708d3cf5ce001288362
Dennis <ostermann3k@googlemail.com>
2026-02-27 11:31:59 +0100

    Fixed tests

--- Kaenx.Konnect.Tests/BusCommonTests.cs
+++ Kaenx.Konnect.Tests/BusCommonTests.cs
@@ -10,7 +10,7 @@ namespace Kaenx.Konnect.Tests
     public class BusCommonTests
     {
         [Fact]
-        public async Task GroupValueWrite_SendsPercentage_false()
+        public async Task GroupValueWrite_Sends_false()
         {
             var endpoint = new IPEndPoint(IPAddress.Parse("192.168.178.167"), 3671);
 
@@ -19,8 +19,7 @@ namespace Kaenx.Konnect.Tests
             var sut = new BusCommon(conn); 
 
             MulticastAddress ga = MulticastAddress.FromString("1/4/53");
-            byte data = 0x00;
-            await sut.GroupValueWrite(ga, data);
+            await sut.GroupValueWrite(ga, false);
         }
 
         [Fact]
@@ -33,7 +32,7 @@ namespace Kaenx.Konnect.Tests
             var sut = new BusCommon(conn);
             MulticastAddress ga = MulticastAddress.FromString("1/4/58");
 
-            byte[] data = new byte[] { 0x33 }; // 20% - DPT 5.001
+            byte data = 0x33; // 20% - DPT 5.001
             await sut.GroupValueWrite(ga, data);
 
             await conn.Disconnect();
